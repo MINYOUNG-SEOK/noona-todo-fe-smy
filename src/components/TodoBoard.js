c4b8023b2126addb388477231cdd6b71608b4007
석민영 <149374648+MINYOUNG-SEOK@users.noreply.github.com>
2024-10-15 17:43:59 +0900

    Update TodoBoard.js

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -1,20 +1,23 @@
 import React from "react";
+import "./TodoBoard.style.css";
 import TodoItem from "./TodoItem";
 
 const TodoBoard = ({ todoList, deleteItem, toggleComplete }) => {
   return (
-    <div>
-      <h2>Todo List</h2>
-      {todoList.length > 0 &&
-        todoList.map((item, index) => (
-          <TodoItem
-            item={item}
-            key={index}
-            deleteItem={deleteItem}
-            toggleComplete={toggleComplete}
-          />
-        ))}
-    </div>
+    <div className="todo-board">
+    {todoList.length > 0 ? (
+      todoList.map((item) => (
+        <TodoItem
+          key={item._id}
+          item={item}
+          deleteItem={deleteItem}
+          toggleComplete={toggleComplete}
+        />
+      ))
+    ) : (
+      <p>할 일이 비어있어요!</p>
+    )}
+  </div>
   );
 };
 
