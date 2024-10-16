d3b4d541be30a90d7485d152d22b99bd09980acd
석민영 <149374648+MINYOUNG-SEOK@users.noreply.github.com>
2024-10-16 16:13:49 +0900

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import "./Header.style.css";
+
+const Header = ({ user, handleLogout, getTasks }) => {
+  const [showLogout, setShowLogout] = useState(false);
+
+  return (
+    <header className="header">
+      <div className="todo-logo">todo</div>
+      {user && (
+        <>
+          <div
+            className="avatar-container"
+            onClick={() => setShowLogout(!showLogout)}
+          >
+            <FontAwesomeIcon icon={faUserCircle} className="avatar" />
+            {showLogout && (
+              <div className="logout-menu" onClick={handleLogout}>
+                <span className="logout-text">→ Sign Out</span>
+              </div>
+            )}
+          </div>
+        </>
+      )}
+    </header>
+  );
+};
+
+export default Header;
