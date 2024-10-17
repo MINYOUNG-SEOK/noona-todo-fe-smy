ab0b09568b1cad65536d4da388bcf765532ddf1d
석민영 <149374648+MINYOUNG-SEOK@users.noreply.github.com>
2024-10-17 15:39:01 +0900

    Update RegisterPage.js

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import api from "../utils/api";
 import "./RegisterPage.style.css";
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const RegisterPage = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [secPassword, setSectPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
+    setLoading(true); 
     try {
       if (password !== secPassword) {
         throw new Error("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
@@ -25,11 +27,16 @@ const RegisterPage = () => {
       }
     } catch (error) {
       setError(error.message);
+    } finally {
+      setLoading(false); 
     }
   };
 
   return (
     <div className="register-page-container">
+       {loading ? (
+      <LoadingSpinner /> 
+    ) : (
       <form className="register-page-form" onSubmit={handleSubmit}>
         <h1>Create Account</h1>
         <p>Get started by creating your new account</p>
@@ -84,7 +91,9 @@ const RegisterPage = () => {
           Register
         </button>
       </form>
+    )}
     </div>
+  
   );
 };
 
